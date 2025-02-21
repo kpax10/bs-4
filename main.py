35bb71f985a7d707bb670f3992febc47bbb75439
kyle <notxap55@yahoo.com>
2025-02-20 16:32:02 -0800

    working

--- main.py
+++ main.py
@@ -7,5 +7,14 @@ yc_webpage = response.text
 soup = BeautifulSoup(yc_webpage, 'html.parser')
 article_tag = soup.find('span', class_='titleline')
 article_title = article_tag.find('a')
-print(article_title.get_text())
 
+# if article_tag:
+article_link = article_title.get('href')
+article_score = article_title.find_next(class_='score')
+print(article_link)
+print(article_score.text)
+
+
+# print(article_title.get_text())
+
+# print(article_title.get('href'))
